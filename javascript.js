37175cf13a431befc8f0786e1c277d71947130c1
Young Ji Kim <youjmik@gmail.com>
2020-12-14 14:25:48 -0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+
+var currentDate = $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+var dailyDate = $("#timeSet")
+
+var saveBtn = $("#saveBtn");
+
+
+function colorDistribute() { 
+
+if (parseInt(currentDate < dailyDate)) {
+
+}
+
+}
