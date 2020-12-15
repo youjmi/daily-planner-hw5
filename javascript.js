b5065a8d8e47825d9e006767632810965c22e16d
Young Ji Kim <youjmik@gmail.com>
2020-12-15 10:47:08 -0500

    changing color change

--- javascript.js
+++ javascript.js
@@ -2,15 +2,29 @@
 
 var currentDate = $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
-var dailyDate = $("#timeSet")
+var dailyHour = moment().format('H')
+console.log(dailyHour)
 
 var saveBtn = $("#saveBtn");
 
 
+
+var timeSet = ["8","9","10","11","12","13","14","15","16","17"]
+
 function colorDistribute() { 
+    
+   for (var i = 0; i < timeSet.length; i++)
+        
+        if (timeSet [i] > dailyHour) {
+            $("<textarea>").addClass("future")
+        }
+        else if (timeSet [i] = dailyHour) {
+            $("<textarea>").addClass("present")
+        }
+        else {
+            $("<textarea>").addClass("past")
+        }
 
-if (parseInt(currentDate < dailyDate)) {
-
-}
-
-}
+    }
+    
+    
