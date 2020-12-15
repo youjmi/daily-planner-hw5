f71bff2d89bc931adcd5d7a498a8655b9baf9b7f
Young Ji Kim <youjmik@gmail.com>
2020-12-15 18:01:15 -0500

    able to make green appear and LS for only 1 time slot..

--- javascript.js
+++ javascript.js
@@ -1,58 +1,66 @@
 
 
 var dailyHour = moment().format('H')
+var currentDate = $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+
+
+var hour8 = $("#timeSet8")
+var hour9 = $("#timeSet9")
+var hour10 = $("#timeSet10")
+var hour11 = $("#timeSet11")
+var hour12 = $("#timeSet12")
+var hour13 = $("#timeSet13")
+var hour14 = $("#timeSet14")
+var hour15 = $("#timeSet15")
+var hour16 = $("#timeSet16")
+var hour17 = $("#timeSet17")
+
+
+
+setInterval (function() {
+    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+},1000)
+
+
+function savePlanner() {
+    $("#calendar-row").each(function () {
+        var id = $(this).attr("id");
+        var schedule = localStorage.getItem(id)
+
+        if (schedule !== null) {
+            $(this).children("textarea").val(schedule)
+        }
+    }
+
+    )}
+savePlanner()
+
 
 var saveBtn = $("#saveBtn");
 
-// var am8 = $("#timeSet8")
-// var am9 = $("#timeSet9")
-// var am10 = $("#timeSet10")
-// var am11 = $("#timeSet11")
-// var pm12 = $("#timeSet12")
-// var pm13 = $("#timeSet13")
-// var pm14 = $("#timeSet14")
-// var pm15 = $("#timeSet15")
-// var pm16 = $("#timeSet16")
-// var pm17 = $("#timeSet17")
-var timeSet = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-colorDistribute();
+saveBtn.on("click", function () {
+    var timeEntry = $(this).siblings("div").text()
+    var textEntry = $(this).siblings("textarea").val();
 
-function colorDistribute() { 
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
-    for (var i = 0; i < timeSet.length; i++)
-         
-         if (timeSet [i] > dailyHour) {
-             $("<textarea>").addClass("future")
-         }
-         else if (timeSet [i] = dailyHour) {
-             $("<textarea>").addClass("present")
-         }
-         else {
-             $("<textarea>").addClass("past")
-         }
- 
-     }
-
-
-function setPlanner() {
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-    $("#calendar-row").each(function () { 
-        var calRow =$(this).attr("calendar-row")
-        localStorage.getItem(calRow)
-
-         if (calRowSave !==null){
-             $(this).children("#text-entry1").val(calRowSave)
-        }
-     })
-}
-
-
-
-saveBtn.on("click",function() {
-    var time = $(this).parent().attr("calendar-row")
-    var textEntry =$(this).siblings("#text-entry1").val();
-
-    localStorage.setItem(time,textEntry);
+    localStorage.setItem(timeEntry, textEntry);
 })
 
 
+
+function colorDistribute() {
+    $("#calendar-row").each(function () {
+        var currentHour = $(this).attr("id")
+
+        if (currentHour > dailyHour) {
+            $(this).addClass("future")
+        }
+        else if (currentHour == dailyHour) {
+            $(this).addClass("present")
+        }
+        else {
+            $(this).addClass("past")
+        }
+    }
+    )}
+colorDistribute()
